597d13b7321bfd7d9e14ec134a4fdce146a1057f
Sean Everest <sean.b.everest@gmail.com>
2017-08-14 08:09:06 -0700

    remove comma

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "google-client-id", "google-secret", scope: 'profile,email', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google', verify_iss: false,
+  provider :google_oauth2, 'google-client-id', 'google-secret', scope: 'profile,email', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google', verify_iss: false
 end
