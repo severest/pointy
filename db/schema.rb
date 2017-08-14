e665487e1d8f22af5680d89bc601f7bd901e0424
Sean Everest <sean.b.everest@gmail.com>
2017-08-14 08:19:14 -0700

    migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema.define(version: 20170803062329) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
@@ -25,44 +25,44 @@ ActiveRecord::Schema.define(version: 20170803062329) do
   end
 
   create_table "game_types", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.boolean  "reverse_points", default: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "reverse_points", default: false
   end
 
   create_table "games", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "game_type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "game_type_id"
     t.index ["game_type_id"], name: "index_games_on_game_type_id"
   end
 
   create_table "people", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "slug"
+    t.string "slug"
     t.index ["slug"], name: "index_people_on_slug", unique: true
   end
 
   create_table "person_games", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "points"
-    t.boolean  "win"
+    t.integer "person_id"
+    t.integer "points"
+    t.boolean "win"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "game_id"
+    t.integer "game_id"
     t.index ["game_id"], name: "index_person_games_on_game_id"
     t.index ["person_id"], name: "index_person_games_on_person_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider",                   null: false
-    t.string   "uid",                        null: false
-    t.string   "name"
-    t.string   "email"
-    t.boolean  "admin",      default: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "name"
+    t.string "email"
+    t.boolean "admin", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
